bdb423d7bb773009bb108fb2833de5e21c0358d6
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-06-21 23:21:11 -0500

    [FEAT] add state to control the input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Form } from "./components/Form"
+import { useState } from 'react';
 
 const App = () => {
 
+  const [value, setValue] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value); 
+  
   return (
     <div className="container">
       <h1> <span>Search Engine</span> whit <span>Debounce Effect</span> </h1>
-      <Form/>
+      <Form {...{value, onChange}}/>
     </div>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,22 @@
-export const Form = () => {
+import { useState } from 'react';
+
+interface Props {
+   value: string;
+   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Form = ({ onChange, value }:Props) => {
+
   return (
-    <form>
+    <>
         <label htmlFor="pokemon">Name of a Pokemon</label>
-        <input type="text" id="pokemon" placeholder="Example: Pikachu" />
-    </form>
+        <input 
+          type="text" 
+          id="pokemon" 
+          placeholder="Example: Pikachu" 
+          value={value}
+          onChange={onChange}
+        />
+    </>
   )
 }
