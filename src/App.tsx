fd093655d5d9c753f9d778d1e29fafedd2386cc7
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-06-21 22:49:46 -0500

    [FEAT] add form to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Form } from "./components/Form"
+
 const App = () => {
+
   return (
-    <div>
+    <div className="container">
       <h1> <span>Search Engine</span> whit <span>Debounce Effect</span> </h1>
+      <Form/>
     </div>
   )
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,8 @@
+export const Form = () => {
+  return (
+    <form>
+        <label htmlFor="pokemon">Name of a Pokemon</label>
+        <input type="text" id="pokemon" placeholder="Example: Pikachu" />
+    </form>
+  )
+}
