3fc21fbacff2c823da57b8fafa1766503fca6b42
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-06-22 11:23:50 -0500

    [FEAT] add debounce effect when the state form value change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { Form } from "./components/Form"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const delay = 1000; // 1s
 
 const App = () => {
 
   const [value, setValue] = useState('');
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value); 
-  
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
+
+    return () => clearTimeout(timer)
+  }, [value, delay]);
+
+
+
   return (
     <div className="container">
       <h1> <span>Search Engine</span> whit <span>Debounce Effect</span> </h1>
-      <Form {...{value, onChange}}/>
+      <Form {...{ value, onChange }} />
     </div>
   )
 }
